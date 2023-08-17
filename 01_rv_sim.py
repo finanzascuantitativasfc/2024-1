45617f8c5b41eca738903a80da6a751a6e1bfd86
Finanzas Cuantitativas FC <69920097+finanzascuantitativasfc@users.noreply.github.com>
2023-08-17 12:05:06 -0600

    Add files via upload
    versión relativa al video 2024-1 Finanzas con Python 03

--- 01_rv_sim.py
+++ 01_rv_sim.py
@@ -10,20 +10,48 @@ import pandas as pd
 import matplotlib as mpl
 import scipy
 import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
 
-degrees_freedom = 5
+# inputs
+coeff = 4 
+# df in student and chi-squared, scale in exponential
 size = 10**6
-random_variable_type = 'student' # normal student uniform
+random_variable_type = 'chi-squared'
+# options: normal student uniform exponential chi-squared
+decimals = 5
 
+
+# code
+str_title = random_variable_type
 if random_variable_type == 'normal':
     x = np.random.standard_normal(size=10**6)
 elif random_variable_type == 'student':
-    x = np.random.standard_t(df=degrees_freedom, size=size)
-# x = np.random.uniform(size=size)
+    x = np.random.standard_t(df=coeff, size=size)
+    str_title = str_title + ' df=' + str(coeff)
+elif random_variable_type == 'uniform':
+    x = np.random.uniform(size=size)
+elif random_variable_type == 'exponential':
+    x = np.random.exponential(scale=coeff, size=size)
+    str_title += ' scale=' + str(coeff)
+elif random_variable_type == 'chi-squared':
+    x = np.random.chisquare(df=coeff, size=size)
+    str_title += ' df=' + str(coeff)
 
-mpl.pyplot.figure()
+mu = np.mean(x)
+sigma = np.std(x)
+skew = skew(x)
+kurt = kurtosis(x)
+
+str_title += '\n' + 'mean=' + str(np.round(mu,decimals)) \
+    + '\n' + 'volatility=' + str(np.round(sigma,decimals)) \
+    + '\n' + 'skewness=' + str(np.round(skew,decimals)) \
+    + '\n' + 'kurtosis=' + str(np.round(kurt,decimals))
+
+# plot
+plt.figure()
 plt.hist(x,bins=100)
-plt.title(random_variable_type)
+plt.title(str_title)
 plt.show()
+
 
 
