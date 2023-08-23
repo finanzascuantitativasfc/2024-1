81bf5c1122e7fd21b7aff5a315704d717d8983b6
Finanzas Cuantitativas FC <69920097+finanzascuantitativasfc@users.noreply.github.com>
2023-08-23 09:29:49 -0600

    Add files via upload
    versión relativa al video 2024-1 Finanzas con Python 04. Primera parte: antes de la refactorización con clases y funciones

--- 01_rv_sim.py
+++ 01_rv_sim.py
@@ -7,16 +7,14 @@ Created on Wed Aug 16 09:33:50 2023
 
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
-import scipy
 import matplotlib.pyplot as plt
-from scipy.stats import tmean, tstd, skew, kurtosis, chi2
+import scipy.stats as st
 
 # inputs
-coeff = 5000
+coeff = 5
 # df in student and chi-squared, scale in exponential
 size = 10**6
-random_variable_type = 'normal'
+random_variable_type = 'student'
 # options: normal student uniform exponential chi-squared
 decimals = 5
 
@@ -37,20 +35,20 @@ elif random_variable_type == 'chi-squared':
     x = np.random.chisquare(df=coeff, size=size)
     str_title += ' df=' + str(coeff)
 
-mu = tmean(x) # tmean
-sigma = tstd(x) # tstd
-skewness = skew(x)
-kurt = kurtosis(x)
+mu = st.tmean(x) # tmean
+sigma = st.tstd(x) # tstd
+skewness = st.skew(x)
+kurtosis = st.kurtosis(x)
 
 # Normality Test: Jarque-Bera
-jb_stat = size/6 * (skewness**2 + 1/4*kurt**2)
-p_value = 1 - chi2.cdf(jb_stat, df=2)
+jb_stat = size/6 * (skewness**2 + 1/4*kurtosis**2)
+p_value = 1 - st.chi2.cdf(jb_stat, df=2)
 is_normal = (p_value > 0.05) # equivalently jb < 6
 
 str_title += '\n' + 'mean=' + str(np.round(mu,decimals)) \
     + ' | ' + 'volatility=' + str(np.round(sigma,decimals)) \
     + '\n' + 'skewness=' + str(np.round(skewness,decimals)) \
-    + ' | ' + 'kurtosis=' + str(np.round(kurt,decimals)) \
+    + ' | ' + 'kurtosis=' + str(np.round(kurtosis,decimals)) \
     + '\n' + 'JB stat=' + str(np.round(jb_stat,decimals)) \
     + ' | ' + 'p-value=' + str(np.round(p_value,decimals)) \
     + '\n' + 'is_normal=' + str(is_normal)
@@ -72,12 +70,12 @@ str_title = 'normal'
 
 while is_normal and n < 500:
     x = np.random.standard_normal(size=10**6)
-    mu = tmean(x) # tmean
-    sigma = tstd(x) # tstd
-    skewness = skew(x)
-    kurt = kurtosis(x)
-    jb_stat = size/6 * (skewness**2 + 1/4*kurt**2)
-    p_value = 1 - chi2.cdf(jb_stat, df=2)
+    mu = st.tmean(x) # tmean
+    sigma = st.tstd(x) # tstd
+    skewness = st.skew(x)
+    kurtosis = st.kurtosis(x)
+    jb_stat = size/6 * (skewness**2 + 1/4*kurtosis**2)
+    p_value = 1 - st.chi2.cdf(jb_stat, df=2)
     is_normal = (p_value > 0.05) # equivalently jb < 6
     print('n=' + str(n) + ' | is_normal=' + str(is_normal))
     n += 1
@@ -85,7 +83,7 @@ while is_normal and n < 500:
 str_title += '\n' + 'mean=' + str(np.round(mu,decimals)) \
     + ' | ' + 'volatility=' + str(np.round(sigma,decimals)) \
     + '\n' + 'skewness=' + str(np.round(skewness,decimals)) \
-    + ' | ' + 'kurtosis=' + str(np.round(kurt,decimals)) \
+    + ' | ' + 'kurtosis=' + str(np.round(kurtosis,decimals)) \
     + '\n' + 'JB stat=' + str(np.round(jb_stat,decimals)) \
     + ' | ' + 'p-value=' + str(np.round(p_value,decimals)) \
     + '\n' + 'is_normal=' + str(is_normal)
@@ -97,4 +95,15 @@ plt.title(str_title)
 plt.show()
 
 
+###############
+# fake homework
+###############
 
+# explanation of the homework goes here
+# e.g. what confidence threshold to use, say 3.75%
+# return is_normal: boolean for the result of Jarque-Bera test
+import scipy.stats as st
+def test_jarque_bera(x):
+    # your code goes here
+    
+    return is_normal
