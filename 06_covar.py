576401d428e9692d147862901246d79c07db2123
Finanzas Cuantitativas FC <69920097+finanzascuantitativasfc@users.noreply.github.com>
2023-10-12 13:06:16 -0600

    Add files via upload
    versión relativa al video 2024-1 Finanzas con Python 26

--- 06_covar.py
+++ 06_covar.py
@@ -21,9 +21,15 @@ importlib.reload(capm)
 #         'XLK','XLF','XLV','XLP','XLY','XLE','XLI']
 # rics = ['^MXX','^SPX','^IXIC', '^STOXX', '^GDAXI', '^FCHI', '^VIX',\
 #         'BTC-USD', 'ETH-USD', 'SOL-USD', 'USDC-USD', 'USDT-USD', 'DAI-USD']
-rics = ['AAPL','MSFT','NVDA','AMZN','GOOG','META','NFLX',\
-        'BRK-B','JPM','V','MA','BAC','MS','GS','BLK',\
-        'LLY','JNJ','PG','MRK','ABBV','PFE']
+# rics = ['SPY','XLK','XLF','XLV','XLE','XLC',\
+#         'XLY','XLP','XLI','XLB','XLRE','XLU']
+rics = ['BTC-USD', 'ETH-USD', 'SOL-USD', 'USDC-USD', 'USDT-USD', 'DAI-USD']
+# rics = ['^SPX','IVW','IVE','QUAL','MTUM','SIZE','USMV',\
+#             'XLK','XLF','XLV','XLP','XLY','XLI','XLC','XLU']
+# rics = ['AAPL','MSFT','NVDA','AMZN','GOOG','META','NFLX',\
+#         'BRK-B','JPM','V','MA','BAC','MS','GS','BLK',\
+#         'LLY','JNJ','PG','MRK','ABBV','PFE']
+# 0-6 \ 7-14 \ 15-20
 
 
 # synchronise all the timeseries of returns
@@ -31,8 +37,34 @@ df = market_data.synchronise_returns(rics)
 
 # compute the variance-covariance and correlation matrices
 mtx = df.drop(columns=['date'])
-mtx_var_covar = np.cov(mtx, rowvar=False)
+mtx_var_covar = np.cov(mtx, rowvar=False) * 252
 mtx_correl = np.corrcoef(mtx, rowvar=False)
 
 # unit test for correlations
 # correl = capm.compute_correlation('^SPX','BTC-USD')
+
+# compute eigenvectors and eigenvalues
+eigenvalues, eigenvectors = np.linalg.eigh(mtx_var_covar)
+variance_explained = eigenvalues / np.sum(eigenvalues)
+prod = np.matmul(eigenvectors, np.transpose(eigenvectors))
+
+##########################
+# PCA for 2D visualisation
+##########################
+
+# compute min and max volatilities
+volatility_min = np.sqrt(eigenvalues[0])
+volatility_max = np.sqrt(eigenvalues[-1])
+
+# compute PCA base for 2D visualisation
+pca_vector_1 = eigenvectors[:,-1]
+pca_vector_2 = eigenvectors[:,-2]
+pca_eigenvalue_1 = eigenvalues[-1]
+pca_eigenvalue_2 = eigenvalues[-2]
+pca_variance_explained = variance_explained[-2:].sum()
+
+# compute min variance portfolio
+min_var_vector = eigenvectors[:,0]
+min_var_eigenvalue = eigenvalues[0]
+min_var_variance_explained = variance_explained[0]
+
