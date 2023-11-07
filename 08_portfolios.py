192cd3e798414b862551caa98dac42507e22a474
Finanzas Cuantitativas FC <69920097+finanzascuantitativasfc@users.noreply.github.com>
2023-11-07 12:15:39 -0600

    Add files via upload
    versión relativa al video 2024-1 Finanzas con Python 32

--- 08_portfolios.py
+++ 08_portfolios.py
@@ -34,6 +34,8 @@ universe = ['^SPX','^IXIC','^MXX','^STOXX','^GDAXI','^FCHI','^VIX',\
             'EURUSD=X','GBPUSD=X','CHFUSD=X','SEKUSD=X','NOKUSD=X','JPYUSD=X','MXNUSD=X']
 rics = random.sample(universe, 5)
 
+print(rics)
+
 # initialise the instance of the class
 port_mgr = portfolio.manager(rics, notional)
 
@@ -43,26 +45,14 @@ port_mgr.compute_covariance()
 # compute the desired portfolios: output class = portfolio.output
 port_min_variance_l1 = port_mgr.compute_portfolio('min_variance_l1')
 port_min_variance_l2 = port_mgr.compute_portfolio('min_variance_l2')
-port_equi_weight = port_mgr.compute_portfolio('equi_weight')
 port_long_only = port_mgr.compute_portfolio('long_only')
-port_markowitz = port_mgr.compute_portfolio('markowitz', target_return=0.1949)
+port_equi_weight = port_mgr.compute_portfolio('equi_weight')
+port_markowitz = port_mgr.compute_portfolio('markowitz', target_return=None)
 
-# return_target = port_long_only.target_return
-return_portfolio_long_only = np.round(port_mgr.returns.dot(port_long_only.weights), 6)
-return_portfolio_equi_weight = np.round(port_mgr.returns.dot(port_equi_weight.weights), 6)
-return_portfolio_markowitz = np.round(port_mgr.returns.dot(port_markowitz.weights), 6)
-
-
-# df = pd.DataFrame()
-# df['rics'] = rics
-# df['returns'] = port_mgr.returns 
-# df['volatilities'] = port_mgr.volatilities 
-# df['markowitz_weights'] = port_markowitz.weights 
-# df['markowitz_allocation'] = port_markowitz.allocation 
-# df['min_variance_weights'] = port_min_variance_l1.weights 
-# df['min_variance_allocation'] = port_min_variance_l1.allocation 
-# df['equi_weight_weights'] = port_equi_weight.weights 
-# df['equi_weight_allocation'] = port_equi_weight.allocation 
-# df['long_only_weights'] = port_long_only .weights 
-# df['long_only_allocation'] = port_long_only .allocation 
+# plot the histograms of returns for the desired portfolio
+port_min_variance_l1.plot_histogram()
+port_min_variance_l2.plot_histogram()
+port_long_only.plot_histogram()
+port_equi_weight.plot_histogram()
+port_markowitz.plot_histogram()
     
--- portfolio.py
+++ portfolio.py
@@ -35,6 +35,7 @@ class manager:
         self.returns = None
         self.volatilities = None
         self.dataframe_metrics = None
+        self.dataframe_timeseries = None
         
     def compute_covariance(self):
         decimals = 6
@@ -52,15 +53,16 @@ class manager:
             volatilities.append(v)
         self.returns = np.array(returns)
         self.volatilities = np.array(volatilities)
-        df = pd.DataFrame()
-        df['rics'] = self.rics
-        df['returns'] = self.returns 
-        df['volatilities'] = self.volatilities
-        self.dataframe_metrics = df
+        df_m = pd.DataFrame()
+        df_m['rics'] = self.rics
+        df_m['returns'] = self.returns 
+        df_m['volatilities'] = self.volatilities
+        self.dataframe_metrics = df_m
+        self.dataframe_timeseries = df
 
         
     def compute_portfolio(self, portfolio_type=None, target_return=None):
-        
+    
         # initial conditions
         x0 = [1 / len(self.rics)] * len(self.rics)
         
@@ -124,6 +126,26 @@ class manager:
         optimal_portfolio.sharpe_ratio =  optimal_portfolio.return_annual / optimal_portfolio.volatility_annual \
             if optimal_portfolio.volatility_annual > 0.0 else 0.0
         
+        # extend dataframe of metrics with optimal weights and allocations
+        df_al = self.dataframe_metrics.copy()
+        df_al['weights'] = optimal_portfolio.weights
+        df_al['allocation'] = optimal_portfolio.allocation
+        optimal_portfolio.dataframe_allocation = df_al
+        
+        # extend dataframe of timeseries with porfolio returns
+        df_ts = self.dataframe_timeseries.copy()
+        rics = list(df_al['rics'])
+        port_rets = df_ts[rics[0]].values * 0.0
+        for ric in rics:
+            df = df_al.loc[df_al['rics'] == ric]
+            w = df['weights'].item()
+            port_rets += df_ts[ric].values * w
+        df_ts['portfolio'] = port_rets
+        optimal_portfolio.dataframe_timeseries = df_ts
+        
+        # compute the remaining metrics for the optimal portfolio
+        optimal_portfolio.compute_stats()
+            
         return optimal_portfolio
         
         
@@ -139,4 +161,42 @@ class output:
         self.return_annual = None
         self.volatility_annual = None
         self.sharpe_ratio = None
+        self.dataframe_allocation = None
+        self.dataframe_timeseries = None
+        self.var_95 = None
+        self.skewness = None
+        self.kurtosis = None
+        self.jb_stat = None
+        self.p_value = None
+        self.is_normal = None
+        
+    def compute_stats(self):
+        x = self.dataframe_timeseries['portfolio'].values
+        self.var_95 = np.percentile(x,5)
+        self.skewness = st.skew(x)
+        self.kurtosis = st.kurtosis(x)
+        self.jb_stat = len(x)/6 * (self.skewness**2 + 1/4*self.kurtosis**2)
+        self.p_value = 1 - st.chi2.cdf(self.jb_stat, df=2)
+        self.is_normal = (self.p_value > 0.05) # equivalently jb < 6
+        
+    def plot_histogram(self):
+        decimals = 4
+        str_title = 'Portfolio = ' + self.type
+        if self.target_return != None:
+            str_title += ' | target return = ' + str(np.round(self.target_return,decimals))
+        str_title += '\n' + 'return_annual=' + str(np.round(self.return_annual,decimals)) \
+            + ' | ' + 'volatility_annual=' + str(np.round(self.volatility_annual,decimals)) \
+            + '\n' + 'sharpe_ratio=' + str(np.round(self.sharpe_ratio,decimals)) \
+            + ' | ' + 'var_95=' + str(np.round(self.var_95,decimals)) \
+            + '\n' + 'skewness=' + str(np.round(self.skewness,decimals)) \
+            + ' | ' + 'kurtosis=' + str(np.round(self.kurtosis,decimals)) \
+            + '\n' + 'JB stat=' + str(np.round(self.jb_stat,decimals)) \
+            + ' | ' + 'p-value=' + str(np.round(self.p_value,decimals)) \
+            + '\n' + 'is_normal=' + str(self.is_normal)
+        plt.figure()
+        x = self.dataframe_timeseries['portfolio'].values
+        plt.hist(x,bins=100)
+        plt.title(str_title)
+        plt.show()
+        
         
